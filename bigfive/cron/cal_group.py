156a40e38550d5b8210d3ef00b34d9dd376395fe
lifengzhi <lifengzhi@buaa.edu.cn>
2019-03-04 21:21:23 +0800

    lfz-2019.3.4

--- bigfive/cron/cal_group.py
+++ bigfive/cron/cal_group.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../')
 from config import *
 from time_utils import *
-from cal_task import group_create
+from cal_main import group_main
 
 def main():
 	query_body = {
@@ -20,12 +20,15 @@ def main():
 	res = es.search(index=GROUP_TASK,doc_type='text',body=query_body)['hits']['hits']
 	if len(res):
 		task = res[0]
+		task_id = task['_id']
 		args_dict = task['_source']['create_condition']
 		keyword = task['_source']['keyword']
 		remark = task['_source']['remark']
 		group_name = task['_source']['group_name']
 		create_time = task['_source']['create_time']
-		group_create(args_dict,keyword,remark,group_name,create_time)
+		es.update(index=GROUP_TASK,doc_type='text',id=task_id,body={'doc':{'progress':1}})
+		group_main(args_dict,keyword,remark,group_name,create_time)
+		es.update(index=GROUP_TASK,doc_type='text',id=task_id,body={'doc':{'progress':2}})
 
 if __name__ == '__main__':
 	main()
