f04140b5b0836ef2e0c24dd0bdf8fa73b1a71a55
Trock <g_trock@163.com>
2019-03-05 16:02:46 +0800

    rock

--- bigfive/cache.py
+++ bigfive/cache.py
@@ -1,7 +1,7 @@
 from flask_cache import Cache
-import redis
 from bigfive.config import REDIS_HOST,REDIS_PORT
 try:
+    import redis
     rds = redis.Redis(host=REDIS_HOST,port=REDIS_PORT)
     rds.ping()
     cache = Cache(config={'CACHE_TYPE': 'redis','CACHE_REDIS_URL':'redis://{REDIS_HOST}:{REDIS_PORT}'.format(REDIS_HOST=REDIS_HOST,REDIS_PORT=REDIS_PORT)})
