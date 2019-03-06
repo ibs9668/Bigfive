2a0f3fcbf6403d3dcbec0bec566deecbd4fbc052
Trock <g_trock@163.com>
2019-03-06 09:18:06 +0800

    rcok

--- bigfive/firstpage/views.py
+++ bigfive/firstpage/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint ,request,jsonify,Response
+from flask import Blueprint,request,jsonify,Response
 import json
 
 from bigfive.firstpage.utils import *
 import os
+
 mod = Blueprint('firstpage',__name__,url_prefix='/firstpage')
 
 
@@ -16,13 +17,13 @@ def test():
 
 @mod.route('/search', methods=['GET', 'POST'])
 def search():
-    keyword = request.args.get('keyword', default='').lower()
+    keyword = request.args.get('keyword', '').lower()
 
-    page = request.args.get('page', default='1')
-    size = request.args.get('size', default='6')
+    page = request.args.get('page', '1')
+    size = request.args.get('size', '6')
 
-    order_name = request.args.get('order_name', default='group_name')
-    order_type = request.args.get('order_type', default='asc')
+    order_name = request.args.get('order_name', 'group_name')
+    order_type = request.args.get('order_type', 'asc')
 
     result = search_group(keyword, page, size, order_name, order_type)
     return json.dumps(result, ensure_ascii=False)
@@ -81,10 +82,12 @@ def bigfive_group_info():
 
 @mod.route('/head', methods=['GET', 'POST'])
 def head():
+    # 个人和群组头像处理
     id = request.args.get('id')
     img_path = 'head_images/' + id + '.jpg'
     mime = 'image/jpeg'
     if not os.path.exists(img_path):
+        # 生成群组头像
         if '_' in id:
             try:
                 image_arrange(id)
--- bigfive/group/utils.py
+++ bigfive/group/utils.py
@@ -2,11 +2,12 @@
 import json
 import re
 
-from bigfive.time_utils import *
 from xpinyin import Pinyin
 
+from bigfive.time_utils import *
 from bigfive.config import es
 from bigfive.cache import cache
+
 
 def index_to_score_rank(index):
     index_to_score_rank_dict = {
@@ -21,7 +22,7 @@ def index_to_score_rank(index):
 
 
 def create_group_task(data):
-    """创建组"""
+    """创建组任务"""
     p = Pinyin()
     data['group_pinyin'] = p.get_pinyin(data['group_name'], '')
     data['create_time'] = nowts()
@@ -53,10 +54,8 @@ def search_group_task(group_name, remark, create_time, page, size, order_name, o
         query['sort'].append({order_name: {"order": order}})
     # 添加组名查询
     if group_name:
-        query['query']['bool']['should'].append(
-            {"wildcard": {"group_name": "*{}*".format(group_name.lower())}})
-        query['query']['bool']['should'].append(
-            {"wildcard": {"keyword": "*{}*".format(group_name.lower())}})
+        query['query']['bool']['should'].append({"wildcard": {"group_name": "*{}*".format(group_name.lower())}})
+        query['query']['bool']['should'].append({"wildcard": {"keyword": "*{}*".format(group_name.lower())}})
     # 添加备注查询
     if remark:
         query['query']['bool']['must'].append(
@@ -74,8 +73,7 @@ def search_group_task(group_name, remark, create_time, page, size, order_name, o
         index = 'group_information'
     else:
         raise ValueError("index is error!")
-    r = es.search(index=index, doc_type='text', body=query, _source_include=[
-        'group_name,create_time,remark,keyword,progress,create_condition'])['hits']['hits']
+    r = es.search(index=index, doc_type='text', body=query, _source_include=['group_name,create_time,remark,keyword,progress,create_condition'])['hits']['hits']
     # 结果为空
     if not r:
         return {}
@@ -83,7 +81,7 @@ def search_group_task(group_name, remark, create_time, page, size, order_name, o
     result = []
     for hit in r:
         item = hit['_source']
-        # 为前端返回es的_id字段,为删除功能做支持
+        # 为前端返回es的_id字段
         item['id'] = hit['_id']
         item['create_time'] = ts2date(item['create_time'])
         result.append(item)
@@ -102,7 +100,6 @@ def delete_by_id(index, doc_type, id):
 
 
 def search_group_ranking(keyword, page, size, order_name, order_type, order_dict):
-
     page = page if page else '1'
     size = size if size else '10'
     sort_list = []
@@ -117,13 +114,13 @@ def search_group_ranking(keyword, page, size, order_name, order_type, order_dict
 
     query = {"query": {"bool": {"must": [{"match_all": {}}], "must_not": [{
         "constant_score": {
-        "filter": {
-        "missing": {
-        "field": "extroversion_label"
+            "filter": {
+                "missing": {
+                    "field": "extroversion_label"
+                }
+            }
         }
-        }
-        }
-        }
+    }
     ], "should": []}}, "from": 0, "size": 6, "sort": [], "aggs": {}}
 
     if keyword:
@@ -184,7 +181,6 @@ def search_group_ranking(keyword, page, size, order_name, order_type, order_dict
 
         hit['_source']['name'] = hit['_source']['group_name']
 
-
         item = hit['_source']
         # 为前端返回es的_id字段,为删除功能做支持
         item['id'] = hit['_id']
@@ -207,7 +203,8 @@ def get_group_user_list(gid):
             }
         }
     }
-    result = es.search(index='group_information', doc_type='text', body=query)['hits']['hits'][0]['_source']['userlist']
+    result = es.search(index='group_information', doc_type='text', body=query)[
+        'hits']['hits'][0]['_source']['userlist']
     return result
 
 
@@ -227,15 +224,19 @@ def get_group_basic_info(gid, remark):
     }
     group_item = {}
     print(query)
-    result = es.search(index='group_information', doc_type='text', body=query)['hits']['hits'][0]['_source']
-    group_ranking_result = es.search(index='group_ranking', doc_type='text', body=query)['hits']['hits'][0]['_source']
+    result = es.search(index='group_information', doc_type='text', body=query)[
+        'hits']['hits'][0]['_source']
+    group_ranking_result = es.search(index='group_ranking', doc_type='text', body=query)[
+        'hits']['hits'][0]['_source']
     print(group_ranking_result)
-    group_item['machiavellianism'] = group_ranking_result['machiavellianism_index']
+    group_item['machiavellianism'] = group_ranking_result[
+        'machiavellianism_index']
     group_item['narcissism'] = group_ranking_result['narcissism_index']
     group_item['psychopathy'] = group_ranking_result['psychopathy_index']
 
     group_item['extroversion'] = group_ranking_result['extroversion_index']
-    group_item['conscientiousness'] = group_ranking_result['conscientiousness_index']
+    group_item['conscientiousness'] = group_ranking_result[
+        'conscientiousness_index']
     group_item['agreeableness'] = group_ranking_result['agreeableness_index']
     group_item['openn'] = group_ranking_result['openn_index']
     group_item['nervousness'] = group_ranking_result['nervousness_index']
@@ -252,34 +253,41 @@ def get_group_basic_info(gid, remark):
     group_item['influence_star'] = group_ranking_result['influence_star']
     group_item['compactness_star'] = group_ranking_result['compactness_star']
 
-
     if remark:
-        es.update(index='group_information', id=gid, doc_type='text', body={'doc': {'remark': remark}})
+        es.update(index='group_information', id=gid,
+                  doc_type='text', body={'doc': {'remark': remark}})
     return group_item
 
 
 def group_preference(group_id):
-    query = {"query":{"bool":{"must":[{"term":{"group_id":group_id}}],"must_not":[],"should":[]}},"from":0,"size":1,"sort":[],"aggs":{}}
-    hits = es.search(index='group_domain_topic',doc_type='text',body=query)['hits']['hits']
-    sta_hits = es.search(index='group_text_analysis_sta', doc_type='text', body=query)['hits']['hits']
+    query = {"query": {"bool": {"must": [{"term": {"group_id": group_id}}], "must_not": [
+    ], "should": []}}, "from": 0, "size": 1, "sort": [], "aggs": {}}
+    hits = es.search(index='group_domain_topic',
+                     doc_type='text', body=query)['hits']['hits']
+    sta_hits = es.search(index='group_text_analysis_sta',
+                         doc_type='text', body=query)['hits']['hits']
     print(query)
     if not hits or not sta_hits:
         return {}
 
     item = hits[0]['_source']
-    domain_static = {one['domain']:one['count'] for one in item['domain_static'] if one['count']}
-    topic_static = {one['topic']:one['count'] for one in item['topic_static'] if one['count']}
+    domain_static = {one['domain']: one['count']
+                     for one in item['domain_static'] if one['count']}
+    topic_static = {one['topic']: one['count']
+                    for one in item['topic_static'] if one['count']}
 
     sta_item = sta_hits[0]['_source']
-    keywords = {one['keyword']:one['count'] for one in sta_item['keywords']}
-    hastags = {one['hastag']:one['count'] for one in sta_item['hastags']}
-    sensitive_words = {one['sensitive_word']:one['count'] for one in sta_item['sensitive_words']}
+    keywords = {one['keyword']: one['count'] for one in sta_item['keywords']}
+    hastags = {one['hastag']: one['count'] for one in sta_item['hastags']}
+    sensitive_words = {one['sensitive_word']: one['count']
+                       for one in sta_item['sensitive_words']}
 
-    result = {'domain_static':domain_static,'topic_result':topic_static, 'keywords': keywords, 'hastags': hastags, 'sensitive_words': sensitive_words}
+    result = {'domain_static': domain_static, 'topic_result': topic_static,
+              'keywords': keywords, 'hastags': hastags, 'sensitive_words': sensitive_words}
     return result
 
 
-def group_influence(group_id,interval):
+def group_influence(group_id, interval):
     query_body = {
         "query": {
             "bool": {
@@ -327,7 +335,8 @@ def group_influence(group_id,interval):
             }
         }
     }
-    es_result = es.search(index="group_influence", doc_type="text", body=query_body)["aggregations"]["groupDate"]["buckets"]
+    es_result = es.search(index="group_influence", doc_type="text", body=query_body)[
+        "aggregations"]["groupDate"]["buckets"]
     result_list = []
     for data in es_result:
         item = {}
@@ -335,7 +344,7 @@ def group_influence(group_id,interval):
         item['influence'] = data['influence']['sum']
         item['activity'] = data['activity']['sum']
         item['importance'] = data['importance']['sum']
-        item['timestamp'] = data['key']//1000
+        item['timestamp'] = data['key'] // 1000
         item['date'] = data['key_as_string']
         result_list.append(item)
     return result_list
@@ -400,6 +409,7 @@ def group_emotion(group_id, interval):
         result["nuetral_line"].append(bucket['nuetral']['sum'])
     return result
 
+
 @cache.memoize(60)
 def group_social_contact(group_id, map_type):
     user_list = es.get(index='group_information', doc_type='text', id=group_id)[
@@ -454,25 +464,28 @@ def group_social_contact(group_id, map_type):
     return {}
 
 
-
 def get_group_activity(group_id):
-    query = {"query":{"bool":{"must":[{"term":{"group_id":group_id}}],"must_not":[],"should":[]}},"from":0,"size":1,"sort":[],"aggs":{}}
-    hits = es.search(index='group_activity',doc_type='text',body=query)['hits']['hits']
+    query = {"query": {"bool": {"must": [{"term": {"group_id": group_id}}], "must_not": [
+    ], "should": []}}, "from": 0, "size": 1, "sort": [], "aggs": {}}
+    hits = es.search(index='group_activity', doc_type='text',
+                     body=query)['hits']['hits']
     if not hits:
         return {}
-    result = {'one':[],'two':[],'three':[],'four':[]}
+    result = {'one': [], 'two': [], 'three': [], 'four': []}
     item = hits[0]['_source']
-    activity_direction = sorted(item['activity_direction'],key=lambda x:x['count'],reverse=True)[:5]
+    activity_direction = sorted(item['activity_direction'], key=lambda x: x[
+                                'count'], reverse=True)[:5]
     for i in activity_direction:
         start_end = i['geo2geo'].split('&')
-        result['one'].append({'start':start_end[0],'end':start_end[1],'count':i['count']})
+        result['one'].append(
+            {'start': start_end[0], 'end': start_end[1], 'count': i['count']})
 
         # s_end
 
     start_geo_item = {}
     end_geo_item = {}
     route_list = []
-    for i in sorted(item['activity_direction'],key=lambda x:x['count'],reverse=True):
+    for i in sorted(item['activity_direction'], key=lambda x: x['count'], reverse=True):
         try:
             if i['geo2geo'].split('&')[0].split(' ')[1] == '其他' or i['geo2geo'].split('&')[1].split(' ')[1] == '其他':
                 continue
@@ -480,22 +493,28 @@ def get_group_activity(group_id):
                 continue
         except:
             continue
-        start_geo_item.setdefault(re.sub(r'省|市|壮族|维吾尔族|回族|自治区', r'', i['geo2geo'].split('&')[0].split(' ')[1]), 0)
-        start_geo_item[re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[0].split(' ')[1])] += i['count']
-        end_geo_item.setdefault(re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[1].split(' ')[1]), 0)
-        end_geo_item[re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[1].split(' ')[1])] += i['count']
-        route_dict = {'s': re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[0].split(' ')[1]), 'e': re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[1].split(' ')[1])}
+        start_geo_item.setdefault(re.sub(r'省|市|壮族|维吾尔族|回族|自治区', r'', i[
+                                  'geo2geo'].split('&')[0].split(' ')[1]), 0)
+        start_geo_item[re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[
+                              0].split(' ')[1])] += i['count']
+        end_geo_item.setdefault(re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i[
+                                'geo2geo'].split('&')[1].split(' ')[1]), 0)
+        end_geo_item[re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[
+                            1].split(' ')[1])] += i['count']
+        route_dict = {'s': re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[0].split(
+            ' ')[1]), 'e': re.sub(r'省|市|壮族|维吾尔族|回族|自治区', '', i['geo2geo'].split('&')[1].split(' ')[1])}
         if route_dict not in route_list:
             route_list.append(route_dict)
 
     geo_item = {}
-    for ks,vs in start_geo_item.items():
+    for ks, vs in start_geo_item.items():
         for ke, ve in end_geo_item.items():
             if ks == ke:
                 geo_item[ks] = vs + ve
                 break
-    result['two'] = sorted(item['main_start_geo'],key=lambda x:x['count'],reverse=True)[:5]
-    result['three'] = sorted(item['main_end_geo'],key=lambda x:x['count'],reverse=True)[:5]
+    result['two'] = sorted(item['main_start_geo'], key=lambda x: x[
+                           'count'], reverse=True)[:5]
+    result['three'] = sorted(item['main_end_geo'], key=lambda x: x[
+                             'count'], reverse=True)[:5]
     result['four'] = {'route_list': route_list, 'geo_count': geo_item}
     return result
-
