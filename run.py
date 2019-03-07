2a55331b8e15ba222ee1292d41471d0da2801329
Trock <g_trock@163.com>
2019-03-07 12:07:21 +0800

    rock

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, Blueprint,session,redirect
+from flask import Flask, render_template, request, Blueprint,session,redirect,make_response
 from optparse import OptionParser
 from bigfive import create_app
 from flask_cors import CORS
@@ -17,7 +17,18 @@ app.secret_key = "ruman"
 #     if not session:
 #         return redirect('manage/login')
 #     return redirect('index/homepage')
-
+@app.after_request
+def af_request(resp):
+    """
+    #请求钩子，在所有的请求发生后执行，加入headers。
+    :param resp:
+    :return:
+    """
+    resp = make_response(resp)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return resp
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0',port=options.port)
