85eed55eec532c18c53ab790b5e049e561eacd1d
Trock <g_trock@163.com>
2019-03-07 13:35:18 +0800

    rock

--- bigfive/__init__.py
+++ bigfive/__init__.py
@@ -15,13 +15,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__)
-    cache.init_app(app)
     app.register_blueprint(colony_mod)
     app.register_blueprint(firstpage_mod)
     app.register_blueprint(person_mod)
     app.register_blueprint(hotevent_mod)
     app.register_blueprint(politics_mod)
     app.register_blueprint(group_mod)
+    cache.init_app(app)
     app.secret_key = "ruman"
     CORS(app, supports_credentials=True)
     return app
