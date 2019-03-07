7943780c7facea29ae397f0174db22410d04bbeb
Trock <g_trock@163.com>
2019-03-07 12:10:44 +0800

    rock

--- run.py
+++ run.py
@@ -17,18 +17,13 @@ CORS(app, supports_credentials=True)
 #     if not session:
 #         return redirect('manage/login')
 #     return redirect('index/homepage')
-@app.after_request
-def af_request(resp):
-    """
-    #请求钩子，在所有的请求发生后执行，加入headers。
-    :param resp:
-    :return:
-    """
-    resp = make_response(resp)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
-    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return resp
+# @app.after_request
+# def af_request(resp):
+#     resp = make_response(resp)
+#     resp.headers['Access-Control-Allow-Origin'] = '*'
+#     resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+#     resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+#     return resp
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0',port=options.port)
