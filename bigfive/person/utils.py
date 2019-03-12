03ebb9c0d5fb942333898adc40d0d201d6b3a705
Trock <g_trock@163.com>
2019-03-12 11:26:30 +0800

    rock

--- bigfive/person/utils.py
+++ bigfive/person/utils.py
@@ -576,11 +576,11 @@ def get_preference_identity(uid):
         if k.startswith('topic_'):
             preference_item[k] = v
     l = sorted(preference_item.items(), key=lambda x:x[1], reverse=True)[0:5]
-    topic_result[topic_dict[l[0][0].replace('topic_', '')]] = int(l[0][1] * 100)
-    topic_result[topic_dict[l[1][0].replace('topic_', '')]] = int(l[1][1] * 100)
-    topic_result[topic_dict[l[2][0].replace('topic_', '')]] = int(l[2][1] * 100)
-    topic_result[topic_dict[l[3][0].replace('topic_', '')]] = int(l[3][1] * 100)
-    topic_result[topic_dict[l[4][0].replace('topic_', '')]] = int(l[4][1] * 100)
+    topic_result[topic_dict[l[0][0].replace('topic_', '')]] = l[0][1]
+    topic_result[topic_dict[l[1][0].replace('topic_', '')]] = l[1][1]
+    topic_result[topic_dict[l[2][0].replace('topic_', '')]] = l[2][1]
+    topic_result[topic_dict[l[3][0].replace('topic_', '')]] = l[3][1]
+    topic_result[topic_dict[l[4][0].replace('topic_', '')]] = l[4][1]
 
     node_main = {'name': labels_dict[preference_and_topic_data['main_domain']], 'id': preference_and_topic_data['uid']}
     node_followers = {'name': labels_dict[preference_and_topic_data['domain_followers']]}
