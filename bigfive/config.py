d0ef803b4d8cfbed37546beba338f239b1dec023
ibs9668 <31496735+ibs9668@users.noreply.github.com>
2019-03-16 23:55:15 +0800

    lwx

--- bigfive/config.py
+++ bigfive/config.py
@@ -126,6 +126,25 @@ POLITICAL_LABELS = ['left','right','mid']
 LEFT_STA = 6000
 RIGHT_STA = 3000
 
+activeness_weight_dict = {'activity_time':0.3, 'activity_geo':0.2, 'statusnum':0.5}
+Fifteenminutes = 60 * 15
+
+TOPIC_WEIGHT_DICT = {'topic_art':0.2,'topic_computer':0.3,'topic_economic':0.5, \
+           'topic_education':0.5,'topic_environment':0.3, 'topic_medicine':0.3,\
+           'topic_military':0.8,'topic_politics':0.8,'topic_sports':0.2,\
+           'topic_traffic':0.3,'topic_life':0.2,'topic_anti_corruption':0.8,\
+           'topic_employment':0.5,'topic_violence':1,\
+           'topic_house':0.4,'topic_law':0.7,'topic_peace':0.9,\
+           'topic_religion':0.8,'topic_social_security':0.6}
+
+IMPORTANCE_WEIGHT_DICT =  {'fansnum':0.2, 'domain':0.5, 'topic':0.3}
+
+#domain weight dict
+
+DOMAIN_WEIGHT_DICT = {'university':0.8, 'homeadmin':0.6, 'abroadadmin':1, 'homemedia':1, \
+                      'abroadmedia':1, 'folkorg':0.8,'lawyer':1, 'politician':0.8,\
+                      'mediaworker':0.8, 'activer':0.6, 'grassroot':0.6, 'other':0.5, 'business':0.6}
+
 
 # 测试用的逻辑"今天"及"一周前"
 today = '2016-11-27'
